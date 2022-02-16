79c8291774ef36337fc605cd9002b145ec1485d6
NeckSnackWrong <david.novickov@gmail.com>
2022-02-16 16:40:00 +0600

    PetShelter start

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,42 +5,9 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static final String DB_USERNAME = "postgres";
-    private static final String DB_PASSWORD = "1593578246";
-    private static final String DB_URL = "jdbc:postgresql://localhost:5432/testDB";
-
     public static void main(String[] args) throws Exception {
-
-        Scanner scanner = new Scanner(System.in);
-        Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-        while (true) {
-            System.out.println("Welcome to My Application");
-            System.out.println("Select option:");
-            System.out.println("1.Get all users");
-            System.out.println("2.Get user by id");
-            System.out.println("3.Create new user");
-            System.out.println("4.Calculate the cost");
-            System.out.println("0.Exit");
-            System.out.println();
-            try {
-                System.out.println("Enter option (1-4):");
-                int option = scanner.nextInt();
-                if (option == 1) {
-
-
-                } else if (option == 2) {
-
-                } else if (option == 3) {
-
-                } else if (option == 4) {
-
-                } else {
-                    break;
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+        PetShelterApplication app = new PetShelterApplication();
+        PetShelterApplication.start();
     }
 
 
---
+++ src/com/company/PetShelterApplication.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class PetShelterApplication {
+    private static final String DB_USERNAME = "postgres";
+    private static final String DB_PASSWORD = "1593578246";
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/testDB";
+
+    public static void start() throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+        Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+        while (true) {
+            System.out.println("Welcome to My Application");
+            System.out.println("Select option:");
+            System.out.println("1.Get all users");
+            System.out.println("2.Get user by id");
+            System.out.println("3.Create new user");
+            System.out.println("4.Calculate the cost");
+            System.out.println("0.Exit");
+            System.out.println();
+            try {
+                System.out.println("Enter option (1-4):");
+                int option = scanner.nextInt();
+                if (option == 1) {
+
+
+                } else if (option == 2) {
+
+                } else if (option == 3) {
+
+                } else if (option == 4) {
+
+                } else {
+                    break;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
